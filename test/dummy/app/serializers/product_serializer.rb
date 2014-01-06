b189938a9eb606bd38e6cda86239e181cba32764
Rahul Trikha <rahul.trikha@gmail.com>
2014-01-06 13:30:53 +1030

    Added thumbnail to product serializer

--- test/dummy/app/serializers/product_serializer.rb
+++ test/dummy/app/serializers/product_serializer.rb
@@ -1,6 +1,10 @@
 class ProductSerializer < ActiveModel::Serializer
 
-  attributes :id, :name
+  attributes :id, :name, :thumb
   has_one :category, embed: :id
+
+  def thumb
+    image_url(object.product_images.first.image.thumb("50x50").url).gsub("/images/", "") unless object.product_images.blank?
+  end
 
 end
